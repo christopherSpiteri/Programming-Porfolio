236d99fc59945e440e9341c05b9e265ba05efaf1
christopherSpiteri <50572568+christopherSpiteri@users.noreply.github.com>
2021-07-25 22:34:15 +0200

    C# Project - Pizza Menu desktop Application
    Firehouse Pizza is a new fast-food outlet employing a novel business 
    concept - customisable pizzas! You are requested to develop software 
    to support the operations of this new shop.
    Menus will lead the customer to choose the pizza size, sauce, crust,
    crust filling (if applicable), and toppings. 
    - Pizza size could be small, medium, and large.
    - Pizza sauce can be tomato or cream.
    - Pizza crust can be regular or stuffed. In case of stuffed crust, 
    the customer would need to choose a crust filling, which can 
    be mozzarella or spiced cheese.
    - Each pizza shall have five different toppings. Hot and spicy 
    toppings have an associated grade which can be mild, moderate, and ferocious.
    The price of each pizza is determined by summing up the prices for the pizza size and its toppings. An extra cost 
    for the crust filling is added where applicable.
    Once all the pizza details are chosen, a summary screen should show an itemised receipt with the total price as 
    shown below. A warning of how hot and spicy the pizza is should also show up when necessary. This value is not 
    cumulative, and is determined by the most hot and spicy topping chosen.

---
+++ HomeAssignmentOOP/HomeAssignmentOOP/CrustedPizza.cs
@@ -0,0 +1,37 @@
+﻿namespace HomeAssignmentOOP
+{
+    public class CrustedPizza : Pizza
+    {
+        string crust;
+
+        public CrustedPizza() : base()
+        {
+            crust = string.Empty;
+        }
+
+        public CrustedPizza(string cSize, string cSauce,string cCrust, Toppings[] ctoppings) : base(cSize,cSauce, ctoppings)
+        {
+            crust = cCrust;
+        }
+        //this method calculated the total price of the pizza and adds up the crust selected to the price
+        public override double getPrice()
+        {
+                double pizzaWithNoCrust = base.getPrice();
+                double cPrice = 0;
+                if (crust == "regular")
+                {
+                    cPrice = 0;
+                }
+                else if (crust == "mozzarella")
+                {
+                    cPrice = 2;
+                }
+                else if (crust == "spice cheese")
+                {
+                    cPrice = 4;
+                }
+                double pizzaPlusCrust = cPrice + pizzaWithNoCrust;
+                return pizzaPlusCrust;
+            }
+        }
+}
